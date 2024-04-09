1e8989ce327b042234b28828c5aa1593f8df038b
steven <brayan.steven.narvaez@correounivalle.edu.co>
2024-04-09 17:25:24 -0500

    documeetnacion back

--- desarrolloBackend/paginaDeInicio/models.py
+++ desarrolloBackend/paginaDeInicio/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.contrib.auth.models import Group, Permission
 
 
-
 class Rol(models.Model):
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField()
@@ -39,3 +38,14 @@ class Usuario(AbstractUser):
 
     groups = models.ManyToManyField(Group, related_name='usuario_set')  # Agrega related_name
     user_permissions = models.ManyToManyField(Permission, related_name='usuario_set')  # Agrega related_name
+    
+    
+    """
+    esta funcinon viene por defecto desde AbstractUser, pero aqui
+    se esta sobreescribiendo el metodo
+    """
+    def get_full_name(self):
+        # Tu lógica personalizada para obtener el nombre completo
+        return f"nombre: {self.apellidos}: Apellido: {self.nombres}"
+    
+    
--- desarrolloBackend/paginaDeInicio/serializers.py
+++ desarrolloBackend/paginaDeInicio/serializers.py
@@ -1,13 +1,6 @@
 from rest_framework import serializers
 from .models import Usuario
 
-class UsuarioSerializer(serializers.ModelSerializer):
-    rol = serializers.SlugRelatedField(read_only=True, slug_field='nombre')
-
-    class Meta:
-        model = Usuario
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'fotografia', 'tipo_identificacion', 'nro_identificacion', 'apellidos', 'nombres', 'genero', 'direccion', 'celular', 'rol')
-
 
 class UsuarioSerializer(serializers.ModelSerializer):
     nombre = serializers.CharField(source='get_full_name', read_only=True)
