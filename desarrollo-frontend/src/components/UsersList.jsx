6921a050ec6218487982f1c9a7542396aa0afc62
Ans-Soft <ana.cantillo@correounivalle.edu.co>
2024-04-10 18:36:21 -0500

    trabajo de ana

--- desarrollo-frontend/src/components/UsersList.jsx
+++ desarrollo-frontend/src/components/UsersList.jsx
@@ -21,52 +21,93 @@ export function UsersList() {
     loadUsuarios()
   }, []);
 
+  const saludar = () => {
+    console.log(usuarios.email, 'hola mundo')
+  }
+
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h2 className="text-2xl font-semibold text-gray-800 mb-6">Lista de Usuarios</h2>
-      {usuarios.length > 0 ? (
-        <div className="overflow-x-auto">
-          <table className="min-w-full bg-white rounded-lg overflow-hidden">
-            <thead>
-              <tr className="w-full bg-gray-100">
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  ID
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Username
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Name
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Apellido
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {usuarios.map((usuario) => (
-                <tr key={usuario.id} className="border-b border-gray-200 hover:bg-gray-50">
-                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                    {usuario.id}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                    {usuario.username || '-'}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                    {usuario.nombres || '-'}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                    {usuario.apellidos || '-'}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+
+
+      <div className="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
+
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
+            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+          </div>
+          <input
+            type="text"
+            id="table-search"
+            className="block p-2 ps-10 text-sm rounded-lg w-80 dark:bg-gray-200"
+            placeholder="Search for items"
+          />
+
         </div>
-      ) : (
-        <p className="text-gray-500">No users found.</p>
-      )}
+
+
+      </div>
+
+
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Id
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Nombres
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Apellidos
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Email
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Rol
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Estado
+            </th>
+            <th scope="col" className="px-6 py-3">
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {usuarios.map((usuario) => (
+            <tr key={usuario.id} className="border-b border-gray-200 hover:bg-slate-200">
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                {usuario.id}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {usuario.first_name || '-'}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {usuario.last_name || '-'}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {usuario.email || '-'}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {usuario.rol || '-'}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {usuario.is_active || '-'}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                <button type="button" className="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900">Editar</button>
+                
+                <button type="button" className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900" 
+                onClick={saludar}
+                >Inhabilitar</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+
     </div>
   );
 }
--- desarrollo-frontend/src/pages/CrudUsersPage.jsx
+++ desarrollo-frontend/src/pages/CrudUsersPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navigation } from '../components/Navigation';
 import { UsersList } from '../components/UsersList';
+import '../css/CrudStyles.css';	
 
 export function CrudUsersPage() {
   return (
@@ -11,7 +12,9 @@ export function CrudUsersPage() {
         </h1>
       </div>
       <Navigation></Navigation>
-      <UsersList></UsersList>
+      <center><div className="w-4/5 container-table-users">
+        <UsersList></UsersList>
+      </div></center>
     </div>
   )
 }
