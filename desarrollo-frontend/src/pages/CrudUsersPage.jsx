48a158d87d08b0b8b41c7fe74a96d4dc4cadd0ed
Ans-Soft <ana.cantillo@correounivalle.edu.co>
2024-04-11 00:16:02 -0500

    Fix styling issues in CrudStyles.css and CrudUsersPage.jsx

--- desarrollo-frontend/src/pages/CrudUsersPage.jsx
+++ desarrollo-frontend/src/pages/CrudUsersPage.jsx
@@ -6,11 +6,6 @@ import '../css/CrudStyles.css';
 export function CrudUsersPage() {
   return (
     <div>
-      <div className="w-full p-5 bg-blue-500 shadow-lg flex items-center justify-center">
-        <h1 className="text-xl font-medium text-white">
-          CRUD DE PRUEBITAS
-        </h1>
-      </div>
       <Navigation></Navigation>
       <center><div className="w-4/5 container-table-users">
         <UsersList></UsersList>
