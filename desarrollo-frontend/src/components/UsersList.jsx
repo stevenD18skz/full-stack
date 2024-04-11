5c8ce9bca6e05769d039a6bc10b15751fae9b3fc
steven <brayan.steven.narvaez@correounivalle.edu.co>
2024-04-11 15:51:22 -0500

    ana aqui esta

--- desarrollo-frontend/src/components/UsersList.jsx
+++ desarrollo-frontend/src/components/UsersList.jsx
@@ -182,3 +182,28 @@ export function UsersList() {
     );
 }
 
+
+/**
+      <div className="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
+
+        <div>
+          <button type="button" className="text-gray-900 hover:text-white border hover:bg-gray-700 font-semibold rounded-full text-sm px-3 py-2 text-center me-2 mb-2 dark:border-gray-600  dark:text-white dark:bg-gray-600">
+            <FontAwesomeIcon icon={faPlus} className="text-indigo-400 h-4 w-4" /> Agregar Usuario
+          </button>
+        </div>
+
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
+            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+          </div>
+          <input
+            type="text"
+            id="table-search"
+            className="button-action block p-2 ps-10 text-sm rounded-lg w-80 dark:bg-gray-200"
+            placeholder="Search for items"
+          />
+
+        </div>
+
+      </div>
+ */
