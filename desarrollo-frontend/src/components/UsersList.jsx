820e519fefa716515d53919a2fbe742e5577e5c5
steven <brayan.steven.narvaez@correounivalle.edu.co>
2024-04-11 22:01:55 -0500

    pull pull pull

--- desarrollo-frontend/src/components/UsersList.jsx
+++ desarrollo-frontend/src/components/UsersList.jsx
@@ -1,113 +1,144 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faPenToSquare, faUserMinus } from '@fortawesome/free-solid-svg-icons';
-import { NavCrudUsers } from './NavCrudUsers';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../css/Modal.css';
 
 
 /*
-EN REALIDAD ES UNA TABLA
+EN REALIDAD ES UINA TABLA
 */
 export function UsersList() {
     const [usuarios, setUsuarios] = useState([]);
+    const [isOpen, setIsOpen] = useState(false);
+    const [formData, setFormData] = useState({});
     const [searchTerm, setSearchTerm] = useState('');
 
+    const openModal = () => {
+        setIsOpen(true);
+    };
 
+    const closeModal = () => {
+        setIsOpen(false);
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Add your logic to handle form submission here
+        console.log(formData);
+    };
 
     useEffect(() => {
-        async function loadUsuarios() {
-            const response = await axios.get('http://127.0.0.1:8000/api/users/')
-            const datosProyectos = response.data.results;
-            setUsuarios(datosProyectos);
-        }
-        loadUsuarios()
-    }, []);
-
-
-
-    const handleChange = (event) => {
-        setSearchTerm(event.target.value);
-        console.log('Searching for:', searchTerm);
+        console.log('pagina cargada')
 
         async function loadUsuarios() {
-            const response = await axios.get(`http://127.0.0.1:8000/api/usuarios/buscar/?nombre=${searchTerm}`)
-            const datosProyectos = response.data;
-            setUsuarios(datosProyectos);
-        }
-
-        async function loadAllUsuarios() {
             const res = await axios.get('http://127.0.0.1:8000/api/users/')
+            console.log(res)
             const datosProyectos = res.data.results;
             setUsuarios(datosProyectos);
         }
 
-        if (searchTerm == "") {
-            loadAllUsuarios()
-        }
-        else {
-            loadUsuarios()
-        }
+        loadUsuarios()
+    }, []);
+
+
+    const filteredUsers = usuarios.filter(user =>
+        user.first_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        user.last_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        user.email.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const handleEditClick = (e) => {
+        e.preventDefault();
+        // Add your logic to handle form submission here
+        console.log('editara');
+    };
+
+
+    const handleEliminarClick = (e) => {
+        console.log("eliminar");
     };
 
 
 
-    const saludar = () => {
-        console.log(usuarios.email, 'hola mundo')
-    }
-
-
-
-    const handleEditClick = () => {
-        console.log(usuarios.email, 'editar')
-    }
-
-
-
-    const handleEliminarClick = () => {
-        console.log(usuarios.username, 'eliminar')
-    }
-
-
-
-
     return (
-        <div className="bg-slate-50 relative overflow-x-auto shadow-md sm:rounded-lg ">
-
-            <div className="">
-
-                <NavCrudUsers></NavCrudUsers>
-
-                <hr className="border border-slate-200 border-solid my-6 w-full" />
-
-                <div className="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
-
-                    <div>
-                        <button type="button" className="text-gray-900 hover:text-white border hover:bg-gray-700 font-semibold rounded-full text-sm px-3 py-2 text-center me-2 mb-2 dark:border-gray-600  dark:text-white dark:bg-gray-600">
-                            <FontAwesomeIcon icon={faPlus} className="text-indigo-400 h-4 w-4" /> Agregar Usuario
-                        </button>
-                    </div>
-
-                    <div className="relative">
-                        <div className="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
-                            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+        <div className=" relative overflow-x-auto shadow-md sm:rounded-lg">
+            <>
+                {isOpen && (
+                    <div className="modal">
+                        <div className="modal-content">
+                            <span className="close" onClick={closeModal}>
+                                &times;
+                            </span>
+                            <form className="p-4 md:p-5" onSubmit={handleSubmit}>
+                                <div className="grid gap-4 mb-4 grid-cols-2">
+                                    <div className="col-span-2">
+                                        <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                                        <input type="text" name="name" id="name" value={formData.name} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required />
+                                    </div>
+                                    <div className="col-span-2 sm:col-span-1">
+                                        <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
+                                        <input type="number" name="price" id="price" value={formData.price} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required />
+                                    </div>
+                                    <div className="col-span-2 sm:col-span-1">
+                                        <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                                        <select id="category" name="category" value={formData.category} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                            <option value="">Select category</option>
+                                            <option value="TV">TV/Monitors</option>
+                                            <option value="PC">PC</option>
+                                            <option value="GA">Gaming/Console</option>
+                                            <option value="PH">Phones</option>
+                                        </select>
+                                    </div>
+                                    <div className="col-span-2">
+                                        <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Description</label>
+                                        <textarea id="description" name="description" rows="4" value={formData.description} onChange={handleChange} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write product description here" />
+                                    </div>
+                                </div>
+                                <button type="submit" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" /></svg>
+                                    Add new product
+                                </button>
+                            </form>
                         </div>
-                        <input
-                            type="text"
-                            id="table-search"
-                            className="button-action block p-2 ps-10 text-sm rounded-lg w-80 dark:bg-gray-200"
-                            placeholder="Search for items"
-                        />
                     </div>
+                )}
+
+            </>
+            <div className="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
+
+                <div>
+                    <button type="button" onClick={openModal} className="text-gray-900 hover:text-white border hover:bg-gray-700 font-semibold rounded-full text-sm px-3 py-2 text-center me-2 mb-2 dark:border-gray-600  dark:text-white dark:bg-gray-600">
+                        <FontAwesomeIcon icon={faPlus} className="text-indigo-400 h-4 w-4" /> Agregar Usuario
+                    </button>
+                </div>
+
+                <div className="relative">
+                    <div className="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
+                        <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+                    </div>
+                    <input
+                        type="text"
+                        id="table-search"
+                        className="button-action block p-2 ps-10 text-sm rounded-lg w-80 dark:bg-gray-200"
+                        placeholder="Search for items"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
 
                 </div>
+
             </div>
 
-
-            <hr className="border border-slate-200 border-solid my-6 w-full" />
-
-
-            <table className=" w-full  text-sm text-left rtl:text-right bg-slate-100">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" className="px-6 py-3">
@@ -133,9 +164,9 @@ export function UsersList() {
                     </tr>
                 </thead>
                 <tbody>
-                    {usuarios.map((usuario) => (
+                    {filteredUsers.map((usuario) => (
                         <tr key={usuario.id} className="border-b border-gray-200 hover:bg-slate-200">
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                                 {usuario.id}
                             </td>
                             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
@@ -151,14 +182,7 @@ export function UsersList() {
                                 {usuario.rol || '-'}
                             </td>
                             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                {usuario.is_active ?
-                                    <div className="flex items-center">
-                                        <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Activo
-                                    </div>
-                                    :
-                                    <div className="flex items-center">
-                                        <div className="h-2.5 w-2.5 rounded-full bg-red-500 me-2"></div> Inhabilitado
-                                    </div>}
+                                {usuario.is_active || '-'}
                             </td>
                             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                 <button
@@ -178,34 +202,6 @@ export function UsersList() {
                     ))}
                 </tbody>
             </table>
-
         </div>
     );
 }
-
-
-/**
- * ANA AQUI ESTA
-      <div className="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
-
-        <div>
-          <button type="button" className="text-gray-900 hover:text-white border hover:bg-gray-700 font-semibold rounded-full text-sm px-3 py-2 text-center me-2 mb-2 dark:border-gray-600  dark:text-white dark:bg-gray-600">
-            <FontAwesomeIcon icon={faPlus} className="text-indigo-400 h-4 w-4" /> Agregar Usuario
-          </button>
-        </div>
-
-        <div className="relative">
-          <div className="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
-            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
-          </div>
-          <input
-            type="text"
-            id="table-search"
-            className="button-action block p-2 ps-10 text-sm rounded-lg w-80 dark:bg-gray-200"
-            placeholder="Search for items"
-          />
-
-        </div>
-
-      </div>
- */
