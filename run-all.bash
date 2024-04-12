20839db29f10a7eac9a14cf7d7ada3504ca68cd0
steven <brayan.steven.narvaez@correounivalle.edu.co>
2024-04-11 23:53:59 -0500

    n

---
+++ run-all.bash
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+# Navega al directorio del backend
+cd desarrollo-backend
+
+# Inicia el servidor Django
+python manage.py runserver
+
+# Navega al directorio del frontend
+cd desarrollo-frontend
+
+# Inicia el servidor de desarrollo React
+npm run dev
