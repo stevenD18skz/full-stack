475e95e18f175f9776e17182488ba65265e5ef7f
Ans-Soft <ana.cantillo@correounivalle.edu.co>
2024-04-11 23:13:28 -0500

    Remove unused CSS files and update import in CrudUsersPage.jsx

--- desarrollo-frontend/src/components/UsersList.jsx
+++ desarrollo-frontend/src/components/UsersList.jsx
@@ -14,13 +14,30 @@ export function UsersList() {
     const [formData, setFormData] = useState({});
     const [searchTerm, setSearchTerm] = useState('');
 
+
+
+    useEffect(() => {
+        async function loadUsuarios() {
+            const response = await axios.get('http://127.0.0.1:8000/api/users/')
+            const datosProyectos = response.data.results;
+            setUsuarios(datosProyectos);
+        }
+        loadUsuarios()
+    }, []);
+
+
+
     const openModal = () => {
         setIsOpen(true);
     };
 
+
+
     const closeModal = () => {
         setIsOpen(false);
     };
+
+
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -30,24 +47,13 @@ export function UsersList() {
         }));
     };
 
+
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Add your logic to handle form submission here
         console.log(formData);
     };
 
-    useEffect(() => {
-        console.log('pagina cargada')
-
-        async function loadUsuarios() {
-            const res = await axios.get('http://127.0.0.1:8000/api/users/')
-            console.log(res)
-            const datosProyectos = res.data.results;
-            setUsuarios(datosProyectos);
-        }
-
-        loadUsuarios()
-    }, []);
 
 
     const filteredUsers = usuarios.filter(user =>
@@ -56,13 +62,14 @@ export function UsersList() {
         user.email.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
+
+
     const handleEditClick = (e) => {
-        e.preventDefault();
-        // Add your logic to handle form submission here
         console.log('editara');
     };
 
 
+    
     const handleEliminarClick = (e) => {
         console.log("eliminar");
     };
@@ -80,17 +87,18 @@ export function UsersList() {
                             </span>
                             <form className="p-4 md:p-5" onSubmit={handleSubmit}>
                                 <div className="grid gap-4 mb-4 grid-cols-2">
+                                    {/* <h3>Crear Usuario</h3> */}
                                     <div className="col-span-2">
-                                        <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                                        <input type="text" name="name" id="name" value={formData.name} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required />
+                                        <label htmlFor="name" className="block mb-2 text-sm font-medium">Nombre de Usuario:</label>
+                                        <input type="text" name="name" id="name" value={formData.name} onChange={handleChange} className="bg-gray-50 border text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Type product name" required />
                                     </div>
                                     <div className="col-span-2 sm:col-span-1">
-                                        <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
-                                        <input type="number" name="price" id="price" value={formData.price} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required />
+                                        <label htmlFor="price" className="block mb-2 text-sm font-medium">Price</label>
+                                        <input type="number" name="price" id="price" value={formData.price} onChange={handleChange} className="bg-gray-50 border text-gray-900 text-sm rounded-lg block w-full p-2.5 " placeholder="$2999" required />
                                     </div>
                                     <div className="col-span-2 sm:col-span-1">
-                                        <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
-                                        <select id="category" name="category" value={formData.category} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                        <label htmlFor="category" className="block mb-2 text-sm font-medium">Category</label>
+                                        <select id="category" name="category" value={formData.category} onChange={handleChange} className="bg-gray-50 border text-gray-900 text-sm rounded-lg block w-full p-2.5">
                                             <option value="">Select category</option>
                                             <option value="TV">TV/Monitors</option>
                                             <option value="PC">PC</option>
@@ -99,11 +107,11 @@ export function UsersList() {
                                         </select>
                                     </div>
                                     <div className="col-span-2">
-                                        <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Description</label>
-                                        <textarea id="description" name="description" rows="4" value={formData.description} onChange={handleChange} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write product description here" />
+                                        <label htmlFor="description" className="block mb-2 text-sm font-medium ">Product Description</label>
+                                        <textarea id="description" name="description" rows="4" value={formData.description} onChange={handleChange} className="block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 dark:placeholder-gray-400" placeholder="Write product description here"/>
                                     </div>
                                 </div>
-                                <button type="submit" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                <button type="submit" className="inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" /></svg>
                                     Add new product
                                 </button>
