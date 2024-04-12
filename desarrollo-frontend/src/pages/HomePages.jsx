cf53e44ae5a5c915a210ffb9be0dd05f839d3559
steven <brayan.steven.narvaez@correounivalle.edu.co>
2024-04-11 23:15:25 -0500

    a

--- desarrollo-frontend/src/pages/HomePages.jsx
+++ desarrollo-frontend/src/pages/HomePages.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Navigation } from '../components/Navigation';
 
-
+//a
 
 export function HomePage() {
     const [name, setName] = useState(''); 
