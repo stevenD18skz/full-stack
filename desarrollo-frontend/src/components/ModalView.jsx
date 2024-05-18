08574d5ee556ec9203f00d7e4314798db4b1c04b
Ans-Soft <ana.cantillo@correounivalle.edu.co>
2024-05-17 21:10:35 -0500

    Update ModalView component to improve user profile display

--- desarrollo-frontend/src/components/ModalView.jsx
+++ desarrollo-frontend/src/components/ModalView.jsx
@@ -14,7 +14,7 @@ export default function ModalView({closeView, formData, setFormData, usuario}) {
 
   return (
     <div className="modal">
-      <div className="modal-content">
+      <div className="modal-content-view">
         {/* <span className="close" onClick={closeView}>
           <FontAwesomeIcon
             icon={faCircleXmark}
@@ -29,13 +29,13 @@ export default function ModalView({closeView, formData, setFormData, usuario}) {
             </div>
           </div>
           <center><h2 className="profile-name">{usuario.first_name.toUpperCase()} {usuario.last_name.toUpperCase()}</h2></center>
-          <center><p classname="about">{usuario.role_user}</p></center>
-          <center><p classname="about">{usuario.email}</p></center>
-          <center><p classname="about">{usuario.phone_user}</p></center>
-          <center><p classname="about">{usuario.doc_type_user} {usuario.doc_number_user} </p></center>
-          <center><p classname="about">{usuario.gender_user}</p></center>
-          <center><p classname="about">{usuario.address_user}</p></center>
-          <center><button classname="btn" onClick={closeView}>Cerrar</button></center>
+          <center><p className="about">{"Rol: " + usuario.role_user}</p></center>
+          <center><p className="about">{"Correo Electronico: " + usuario.email}</p></center>
+          <center><p className="about">{"Numero Telefónico: " + usuario.phone_user}</p></center>
+          <center><p className="about">{"N° Identificación: " + usuario.doc_number_user} </p></center>
+          <center><p className="about">{"Genero: " + usuario.gender_user}</p></center>
+          <center><p className="about">{"Dirección: " + usuario.address_user}</p></center>
+          <center><button className="btn" onClick={closeView}>Cerrar</button></center>
         </div>
       </div>
     </div>
